160aac245bf2862d22be4525dd227cec0d00b9be
Damien Thenot <damien.thenot@gmail.com>
2016-04-20 12:33:29 +0200

    Quelques ajouts dans ProjetOS.c, Ajout de la création des processus chefs d'équipe, il manque de leur envoyer le nom du fichier leur correspondant. Modification de gen_file pour qu'il génère des floats à la place des int

--- ProjetOS.c
+++ ProjetOS.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <string.h>
+#include <assert.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #define N 256
 
@@ -10,23 +14,29 @@ struct byThread{
 	int taille;
 	int deb;
 	int fin;
+	char * mode;
 	pthread_mutex_t * mutex;
 };
 
-struct byThread * creationByThread(float * chiffre, int deb, int fin, pthread_mutex_t * mutex){
+struct byThread * creationByThread(float * chiffre, int deb, int fin, char * mode, pthread_mutex_t * mutex){
+	//Initialise la structure de données byThread qui sert au processus chefs d'équipes à passer les données que doivent traiter les threads aux différents threads qu'il engendre.
+	assert(deb>=0);
+	assert(fin>=0);
 	struct byThread * thInfo = malloc(sizeof(struct byThread));
 	thInfo->deb = deb;
 	thInfo->fin = fin;
 	thInfo->chiffre = chiffre;
 	thInfo->mutex = mutex;
+	thInfo->mode = mode;
 	return thInfo;
 }
 
 int main(int argc, char ** argv){
 	if(argc>=3){
 		//~ fprintf(stdout, "こんにちは世界!\n");
-		char nomFichiers[N][N]; int i, j, nombreFichiers = 0;
-		char mode[N];
+		char nomFichiers[N][N]; int i, j, nombreFichiers = 0;//nomFichiers contient les noms des fichiers d'entrée qu'il faudra faire passer aux processus chefs d'équipe.
+		//nombreFichiers est la taille max de la première dimension de nomFichiers, c'à-d le nombre de nom de fichier et le nombre de fichier qu'on a.
+		char mode[N];//mode est le mode, c'à-d "max", "min", "avg" etc...
 		
 		i = 0, j= 2;
 		while(i<N && j<argc){
@@ -35,20 +45,43 @@ int main(int argc, char ** argv){
 			i++; j++;
 		}
 		strcpy(mode, argv[1]); //On récupère le mode d'utilisation entrée dans mode pour utiliser plus tard.
-		
-		
-		/*pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-		float chiffre[N];
-		chiffre[0] = 0.0;
-		for(i = 1; i<N; i++){
-			chiffre[i] = chiffre[i-1]+1.7;
+				
+		pid_t pid;
+		for(i = 0; i<nombreFichiers; i++){
+			pid = fork();
+			//TODO: Il faudrait choisir ici quels fichiers sera passer au processus fils(ou processus chefs d'équipes.
+			if(pid){
+				//pere
+				wait(NULL);				
+			}
+			else{
+				//fils
+				break;
+			}
 		}
-		struct byThread * thInfo = creationByThread(chiffre, 2, 3, &mutex);*///Façon d'initialiser un struct byThread. C'est la structure qu'il faudra envoyer aux threads. Elles seront initialisé par les processus chefs d'équipe.
+		if(!(pid)){
+			fprintf(stdout, "Fils\n");
+			//TODO: Créer threads ici en fonction des entrées.
+		}
+		
+		//PERE
+		//Le père doit procréer autant qu'il y a de fichiers d'entrées.		
+		//FILS
+		//doit assigner les valeurs de sont fichiers aux différents threads et les créer.
+		
+		//~ pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+		//~ float chiffre[N];
+		//~ chiffre[0] = 0.0;
+		//~ for(i = 1; i<N; i++){
+			//~ chiffre[i] = chiffre[i-1]+1.7;
+		//~ }
+		//~ struct byThread * thInfo = creationByThread(chiffre, 2, 3, mode, &mutex); //Façon d'initialiser un struct byThread. C'est la structure qu'il faudra envoyer aux threads. Elles seront initialisé par les processus chefs d'équipe.
 		
 		//Affichage des noms de fichier en entrée (affichage test).
 		for(i = 0; i<nombreFichiers; i++){
 			fprintf(stdout, "%s ", nomFichiers[i]);
 		}
+		fprintf(stdout, "\n");
 		
 		
 	}
--- gen_file.py
+++ gen_file.py
@@ -1,14 +1,14 @@
 #coding: utf8
 import random as rand
 nom = raw_input("Entrer le nom du fichier : ")
-nbVal = input("Entrer le nombre de valeur que doit contenir le fichier : ")
+#~ nbVal = input("Entrer le nombre de valeur que doit contenir le fichier : ")
 
 fw = open(nom, "w")
-
+nbVal = rand.randint(0, 100000)
 fw.write(str(nbVal)+"\n")
 i = 0
 while(i<nbVal):
-	fw.write(str(rand.randint(0, 1000000))+"\n")
+	fw.write(str(rand.uniform(0.0, 1000000.0))+"\n")
 	i+=1
 fw.close()
 print("Fichier généré.")
